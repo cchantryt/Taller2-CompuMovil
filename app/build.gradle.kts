793cb27c14cbb12781850e79bd624cc72d34f0f1
Jorge Chantryt <jcamilo-chantryt@javeriana.edu.co>
2023-10-07 15:09:56 -0500

    Inicio Actividad Mapa (no funcional)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,6 @@ dependencies {
 
     implementation("com.google.android.gms:play-services-maps:18.1.0")
     implementation("com.google.android.gms:play-services-location:21.0.1")
+
+    implementation("com.google.android.gms:play-services-location:21.0.1")
 }
