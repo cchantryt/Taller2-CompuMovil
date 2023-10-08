685b64f0b4e1e3c520a4be81bbbeda732521e937
Jorge Chantryt <jcamilo-chantryt@javeriana.edu.co>
2023-10-07 19:30:01 -0500

    Permisos de localizacion agregados y boton mapa funcional

--- app/src/main/java/com/ch/taller2/MainActivity.kt
+++ app/src/main/java/com/ch/taller2/MainActivity.kt
@@ -1,23 +1,45 @@
 package com.ch.taller2
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.ch.taller2.databinding.ActivityMainBinding
+import android.Manifest
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
+    val PERM_LOCATION_CODE = 303
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //Camara
         binding.botonCamara.setOnClickListener(){
             startActivity(Intent(this, CamaraActivity::class.java))
         }
+        //Mapa
         binding.botonMapa.setOnClickListener(){
-            startActivity(Intent(this, MapaActivity::class.java))
+            //Solicitamos permisos de ubicacion
+            when {
+                ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    //Toast.makeText(this, "mapa", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this, MapaActivity::class.java))
+                }
+                shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                    Toast.makeText(this, "El permiso de ubicacion es necesario", Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+                    requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PERM_LOCATION_CODE)
+                }
+            }
+
         }
 
     }
--- app/src/main/java/com/ch/taller2/MapaActivity.kt
+++ app/src/main/java/com/ch/taller2/MapaActivity.kt
@@ -1,14 +1,25 @@
 package com.ch.taller2
 
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.ch.taller2.databinding.ActivityMapaBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import android.Manifest
 
-
-class MapaActivity : AppCompatActivity() {
+class MapaActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMapaBinding
+    private lateinit var map: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,11 +29,32 @@ class MapaActivity : AppCompatActivity() {
         // Obtener el fragmento del mapa
         val mapFragment = SupportMapFragment.newInstance()
         supportFragmentManager.beginTransaction()
-            .replace(binding.mapContainer.id, mapFragment)
+            .replace(R.id.mapContainer, mapFragment)
             .commit()
 
-        mapFragment.getMapAsync { googleMap ->
-            // Configurar el mapa
+        mapFragment.getMapAsync(this)
+
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location: Location? ->
+                    location?.let {
+                        val userLocation = LatLng(it.latitude, it.longitude)
+                        map.addMarker(MarkerOptions().position(userLocation).title("Mi Ubicación"))
+                        map.moveCamera(CameraUpdateFactory.newLatLng(userLocation))
+                        map.animateCamera(CameraUpdateFactory.zoomTo(15f))
+                    }
+                }
         }
     }
 }
