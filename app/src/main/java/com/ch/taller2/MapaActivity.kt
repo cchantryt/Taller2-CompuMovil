c87c8cf7dbce6d6ce00e2417ee95a1e049d46e9c
Jorge Chantryt <jcamilo-chantryt@javeriana.edu.co>
2023-10-08 09:56:30 -0500

    Mapa reactivo al sensor de luminosidad

--- app/src/main/java/com/ch/taller2/MapaActivity.kt
+++ app/src/main/java/com/ch/taller2/MapaActivity.kt
@@ -15,6 +15,14 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import android.Manifest
+import android.content.Context
+import android.content.res.Resources
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.util.Log
+import com.google.android.gms.maps.model.MapStyleOptions
 
 class MapaActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMapaBinding
@@ -39,6 +47,7 @@ class MapaActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     /*------------------------------------------------------------ FUNCIONES MAPA ------------------------------------------------------------*/
+    //Mapa ready
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
 
@@ -64,4 +73,74 @@ class MapaActivity : AppCompatActivity(), OnMapReadyCallback {
                 }
         }
     }
+    //Registramos los datos del sensor de luminosidad
+    override fun onResume() {
+        super.onResume()
+        registerLightSensorListener()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unregisterLightSensorListener()
+    }
+
+    /*------------------------------------------------------------ FUNCIONES SENSOR LUMINOSIDAD ------------------------------------------------------------*/
+    private val sensorManager: SensorManager by lazy {
+        getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
+    private val lightSensor: Sensor? by lazy {
+        sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+    }
+
+    private fun registerLightSensorListener() {
+        lightSensor?.let {
+            sensorManager.registerListener(lightSensorListener, it, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+
+    private fun unregisterLightSensorListener() {
+        sensorManager.unregisterListener(lightSensorListener)
+    }
+
+    //Listener para el sensor de luminosidad
+    private val lightSensorListener = object : SensorEventListener {
+        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+
+        override fun onSensorChanged(event: SensorEvent?) {
+            event?.let {
+                // Obtén el valor de la luminosidad desde el sensor
+                val luminosity = event.values[0]
+
+                // Actualiza el estilo del mapa según la luminosidad
+                updateMapStyle(luminosity)
+            }
+        }
+    }
+
+    //Umbral de luminosidad
+    companion object {
+        private const val LUMINOSITY_THRESHOLD = 700.0
+    }
+
+    //Funcion para actualizar el estilo del mapa
+    private fun updateMapStyle(luminosity: Float) {
+        val style = if (luminosity < LUMINOSITY_THRESHOLD) {
+            // Estilo oscuro para baja luminosidad
+            R.raw.map_day
+        } else {
+            // Estilo claro para alta luminosidad
+            R.raw.map_night
+        }
+
+        try {
+            val success = map.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, style))
+            if (!success) {
+                Log.e("MapaActivity", "Fallo al aplicar estilo de mapa.")
+            }
+        } catch (e: Resources.NotFoundException) {
+            Log.e("MapaActivity", "No se pudo encontrar el estilo de mapa. Error: $e")
+        }
+    }
+
 }
